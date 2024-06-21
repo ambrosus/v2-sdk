af80eab21a97e2e2fba77668293ef9633de63ad6
OlehParyshkura2 <o.paryshkura@inc4.net>
2024-06-21 08:51:10 +0300

    update constants factory address

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import { Percent } from '@airdao/sdk-core'
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x31A65bade6593B4fab076c6b16c338182abcC8b7'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x0bb23e27e38d82e8f62fcca75da5a51c07f86abce63a1c0673a8e1a0cbd40fbe'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
